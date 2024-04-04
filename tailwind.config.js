35b5a78cff96542131c5fb1f0b3380c29034afd6
Haabiy <abiy.dema@gmail.com>
2024-04-04 18:23:34 +0200

    Pricing UI done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./dist/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        '75': '28rem',
+        '80': '30rem',
+      },
+
+      width:{
+        '35': '18rem',
+        '40': '20rem',
+      }
+
+    },
   },
   plugins: [],
 }
