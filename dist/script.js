b58028e7d4f7e425c7658c283a44b67a2aa75a10
Haabiy <abiy.dema@gmail.com>
2024-05-25 17:55:44 +0200

    added - Hamburger menu

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,10 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('show');
-});
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
 
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
